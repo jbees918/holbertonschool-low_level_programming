d45fb0078aefe66c346b59a38a7338c861ef8c8d
jbees918 <5590@holbertonstudents.com>
2022-10-03 15:23:52 -0700

    string cat

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,18 @@
+#ifndef holberton_h
+#define holberton_h
+
+/**
+ * holberton - holberton.h
+ *
+ * Description: header file containing the prototypes
+ * for all the functions in 0x06. C - more pointers, arrays
+ * and strings.
+ */
+
+#include <stdio.h>
+#include <ctype.h>
+
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+#endif
