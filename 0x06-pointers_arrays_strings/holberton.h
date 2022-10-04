b07a884b68aaab2d07dd6252b071e73b7dbdbec2
jbees918 <5590@holbertonstudents.com>
2022-10-04 15:20:26 -0700

    comparison

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -12,6 +12,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
+int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
