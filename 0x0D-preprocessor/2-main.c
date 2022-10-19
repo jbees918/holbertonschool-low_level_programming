99835f970678e22449e194d9bb0624341aa9b2e8
jbees918 <5590@holbertonstudents.com>
2022-10-18 17:25:49 -0700

    file macro

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - program prints name of file it was compiled from
+ * followed by new line
+ * 
+ * return: 0
+ */
+int main(void)
+{
+	print("%s\n", __FILE__);
+
+	return (0);
+}
