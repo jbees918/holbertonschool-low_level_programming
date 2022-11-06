c56b7fa272b466c0375e2652741e3dc0d27b4730
jbees918 <5590@holbertonstudents.com>
2022-11-05 20:35:12 -0700

    list length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * list_len - function to return number
- * of elements in linked list_t list
+ * list_len - function that returns the number
+ * of elements in a linked list_t list.
  *
  * @h: given pointer
  *
- * return: number of elements
+ * Return: number of elements
  */
 size_t list_len(const list_t *h)
 {
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
-
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 #include <string.h>
+
+/* struct provided for project */
 
 /**
  * struct list_s - singly linked list
@@ -13,7 +16,6 @@
  *
  * Description: singly linked list node structure
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -21,12 +23,13 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
+/* prototypes */
+
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 
 #endif
