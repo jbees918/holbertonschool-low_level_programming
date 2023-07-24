d5b92cf028e4adcb3a812f4d5753ec659159760a
jbees918 <5590@holbertonstudents.com>
2023-07-24 17:00:51 -0500

    code change

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
+
 /**
-*print_dlistint - prints all elements of doubly linked list
-*@h: pointer to head node
-*Return: number of nodes
-*/
+ * print_dlistint - prints int struct vals of dll to SO
+ * @h: pointer to first node in dll
+ * Return: number of nodes in dll
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t numNodes = 0;
 
+	/* prints node int, adds to node count, then targets next node */
 	while (h)
 	{
 		printf("%d\n", h->n);
+		numNodes++;
 		h = h->next;
-		nodes++;
 	}
-	return (nodes);
+	return (numNodes);
 }
