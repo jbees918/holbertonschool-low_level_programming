d13239168567fb87937631d73a79928612277ef7
jbees918 <5590@holbertonstudents.com>
2023-08-14 15:01:39 -0500

    linear fix

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,27 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "search_algos.h"
 
 /**
- * linear_search - Search for a value in an array using linear search.
- * @array: Pointer to the first element of the array.
- * @size: Number of elements in the array.
- * @value: Value to search for.
+ * linear_search - Search Array for Element value
+ * @array: Array to search
+ * @size: Size of array
+ * @value: Value to be found
  *
- * Return: Index of the first occurrence of 'value', or -1 if not found.
- */
+ * Return: Index of first Value or -1
+*/
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-        return -1;
+	size_t i = 0;
 
-   size_t i;
-for (i = 0; i < size; i++)
-    {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value)
-            return (int)i;
-    }
+	if (array == NULL)
+		return (-1);
 
-    return -1; /** Value not found */
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%li] = [%i]\n", i, array[i]);
+		if (array[i] == value)
+			return ((int)i);
+	}
+
+	return (-1);
 }
