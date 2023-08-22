86509321587c4f0049bf47b5806ac1934bb81300
jbees918 <5590@holbertonstudents.com>
2023-08-21 19:41:57 -0500

    header update

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,11 +1,38 @@
-#ifndef SEARCHALGO
-#define SEARCHALGO
+#ifndef SEARCH_ALGOS
+#define SEARCH_ALGOS
 
-#include <stddef.h>
-#include <stdlib.h>
+/* include libraries */
+
 #include <stdio.h>
+#include <stdlib.h>
+
+/*  structs */
+
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of node in list
+ * @next: Pointer to next node
+ * @express: Pointer to next node in exp lane
+ *
+ * Description: singly linked list node structure with express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
+
+
+/* functions declrations */
 
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-
-#endif
+#endif /* SEARCH_ALGOS */
